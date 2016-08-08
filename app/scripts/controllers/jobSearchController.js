8ba9073faf0894f1f569a1cc5148ac48d3adad22
Sina E <sinaizad@gmail.com>
2016-08-08 13:14:05 +0300

    Update CP columns based on specification provided by Andrea

--- app/scripts/controllers/jobSearchController.js
+++ app/scripts/controllers/jobSearchController.js
@@ -76,16 +76,16 @@ angular.module('sampleApp4App')
       });
 
       var columns = [
-        { dataField: 'TaskNumber',          caption: locale.getString('common.CP_JobsGrid_TaskNumber'), visible: false},
-        { dataField: 'CustomerCode',        caption: locale.getString('common.CP_JobsGrid_CustomerCode') },
+        { dataField: 'TaskNumber',          caption: locale.getString('common.CP_JobsGrid_TaskNumber')},
         { dataField: 'CompanyName',         caption: locale.getString('common.CP_JobsGrid_CompanyName') },
+        { dataField: 'CustomerCode',        caption: locale.getString('common.CP_JobsGrid_CustomerCode'), visible: false },
         { dataField: 'TaskCreatedDateTime', caption: locale.getString('common.CP_JobsGrid_TaskCreatedDateTime'), dataType: 'date', format: 'shortDateShortTime'},
         { dataField: 'TaskScheduledDateTime',   caption: locale.getString('common.CP_JobsGrid_TaskScheduledDateTime'), dataType: 'date', format: 'shortDateShortTime' },
+        { dataField: 'TaskCompletedDateTime',   caption: locale.getString('common.CP_JobsGrid_TaskCompletedDateTime'), dataType: 'date', format: 'shortDateShortTime' },
+        { dataField: 'TaskDescription',      caption: locale.getString('common.CP_JobsGrid_TaskDescription') },
         { dataField: 'TaskPriorityDescription', caption: locale.getString('common.CP_JobsGrid_TaskPriorityDescription') },
         { dataField: 'TaskStatusCode',      caption: locale.getString('common.CP_JobsGrid_TaskStatusCode') },
-        { dataField: 'EquipmentId',         caption: locale.getString('common.CP_JobsGrid_EquipmentId') },
-        { dataField: 'TechnicianCode',      caption: locale.getString('common.CP_JobsGrid_TechnicianCode') },
-        { dataField: 'TechnicianName',      caption: locale.getString('common.CP_JobsGrid_TechnicianName') }
+        { dataField: 'EquipmentId',         caption: locale.getString('common.CP_JobsGrid_EquipmentId') }
       ];
 
       $scope.jobsGrid = {
@@ -99,7 +99,7 @@ angular.module('sampleApp4App')
         stateStoring: {
           enabled: true,
           type: 'localStorage',
-          storageKey: 'gridState'
+          storageKey: 'jobsGridState'
         },
         columnChooser: { enabled: true },
         allowColumnReordering: true,
